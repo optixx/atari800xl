639fd541e35d2f1e8bf9125dbd1ea16b3613f583
David Voswinkel <david@userlike.com>
2023-05-06 10:04:58 +0200

    Add dli7 example

---
+++ dli7/dli7.asm
@@ -0,0 +1,53 @@
+    run     start
+    org     $0600
+
+WSYNC       = $d40a
+VCOUNT      = $d40b
+COLPF2      = $d018
+TMP         = $ce
+DL          = $cc
+
+start
+    lda     $0230
+    sta     DL
+    lda     $0231
+    sta     DL+1
+    lda     #<dli
+    sta     $0200
+    lda     #>dli
+    sta     $0201
+
+    ldy     #3
+    lda     (DL),y
+    eor     #128
+    sta     (DL),y
+    ldy     #8
+dlloop
+    lda     (DL),y
+    eor     #128
+    sta     (DL),y
+    iny
+    cpy     #29
+    bne     dlloop
+    lda     #192
+    sta     $d40e
+
+main
+    lda     #$ff
+    sta     $02fc 
+loop
+    ldx     $02fc
+    cpx     #$ff
+    bne     exit
+    jmp     loop
+exit
+    rts
+
+dli
+    pha
+    lda     VCOUNT
+    sta     WSYNC 
+    sta     COLPF2
+    pla
+    rti 
+
