ce79e2f2d85df4bfbd4cf4985c2d6aa3723fa17a
David Voswinkel <david@optixx.org>
2024-09-09 16:46:46 +0000

    Rely on atari800 installed by homebrew

--- scripts/run_atari_rom.sh
+++ scripts/run_atari_rom.sh
@@ -1,3 +1,3 @@
-#!/bin/sh 
+#!/bin/sh
 ATARI_PATH="/Users/david/Devel/arch/atari"
-${ATARI_PATH}/bin/atari800 -pal -xe -xlxe_rom ${ATARI_PATH}/roms/ATARIXL.ROM -video-accel -win-width 800 -win-height 600 "$1"
+atari800 -pal -xe -xlxe_rom ${ATARI_PATH}/roms/ATARIXL.ROM -video-accel -win-width 800 -win-height 600 "$1"
