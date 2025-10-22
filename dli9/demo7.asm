56abb3a1bea498280ad3777a7887413667ef39ad
David Voswinkel <david@optixx.org>
2025-10-22 15:59:40 +0000

    Add dli10 in fastbasic

---
+++ dli9/demo7.asm
@@ -0,0 +1,12 @@
+;**********************************
+;* DISPLAY-LIST-INTERRUPTS
+;* Vielfarbiges GRAPHICS 0 Display-Lis
+;**********************************
+
+COLPF2    EQU   $D01B
+WSYNC     EQU   $D40A
+
+
+    run     start
+    org     $0600
+start
